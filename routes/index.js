07e5247566b5f8f5becd8541db41ff029c37d9c7
mavo97 <mavodeveloper@gmail.com>
2020-06-25 13:23:01 -0500

    SignIn and SignUp

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@
 
 const express = require('express')
 const api = express.Router()
+const auth = require('../middlewares/auth')
 const productCtrl = require('../controllers/product')
 
 
@@ -14,5 +15,9 @@ api.post('/product', productCtrl.saveProduct)
 api.put('/product/:productId', productCtrl.updateProduct)
 
 api.delete('/product/:productId', productCtrl.deleteProduct)
+
+api.get('/private', auth, function(req, res) {
+    res.status(200).send({ message: 'Tienes acceso!' })
+})
 
 module.exports = api
